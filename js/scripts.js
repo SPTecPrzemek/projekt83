7d346e75f68f96c619dc618c10808cd33bdf3766
Stasiak Przemyslaw <stasiakprzemyslaw@MacBook-Pro-Stasiak.local>
2017-02-23 23:13:28 +0100

    Zrobione zadanie

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by stasiakprzemyslaw on 23.02.2017.
+ */
+var triangleArea;
+
+var a = prompt('Podaj podstawę trójkąta A : ');
+var h = prompt('Podaj wysokość trójkąta  H : ');
+
+triangleArea = a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea);
